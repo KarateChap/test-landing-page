ee617ec53b28739084507d86501eac4a288b9413
Ralph Gabriel Mariano <gabrielmarianoofficial@gmail.com>
2025-02-21 13:30:05 +0800

    add more sections

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,4 +1,5 @@
 import CheckIcon from "@/assets/check.svg";
+import { clsx } from "clsx";
 
 const pricingTiers = [
   {
@@ -54,14 +55,16 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section className="py-24">
+    <section className="py-24 bg-white">
       <div className="container">
-        <h2 className="section-title">Pricing</h2>
-        <p className="section-description mt-5">
-          Free forever. Upgrade for unlimited tasks, better security, and
-          exclusive features.
-        </p>
-        <div>
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and
+            exclusive features.
+          </p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
           {pricingTiers.map(
             ({
               title,
@@ -71,8 +74,31 @@ export const Pricing = () => {
               inverse,
               features,
             }) => (
-              <div className="p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA]">
-                <h3 className="text-lg font-bold text-black/50">{title}</h3>
+              <div
+                className={clsx(
+                  "card",
+                  inverse
+                    ? "border-black bg-black text-white"
+                    : "border-[#F1F1F1]"
+                )}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={clsx(
+                      "text-lg font-bold",
+                      inverse ? "text-white/60" : "text-black/50"
+                    )}
+                  >
+                    {title}
+                  </h3>
+                  {popular === true && (
+                    <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
+                      <span className="bg-[linear-gradient(to_right,#DD7DDF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium">
+                        Popular
+                      </span>
+                    </div>
+                  )}
+                </div>
                 <div className="flex items-baseline gap-1 mt-[30px]">
                   <span className="text-4xl font-bold tracking-tighter leading-none">
                     ${monthlyPrice}
@@ -81,7 +107,12 @@ export const Pricing = () => {
                     /month
                   </span>
                 </div>
-                <button className="btn btn-primary w-full mt-[30px]">
+                <button
+                  className={clsx(
+                    "btn btn-primary w-full mt-[30px]",
+                    inverse ? "bg-white text-black" : ""
+                  )}
+                >
                   {buttonText}
                 </button>
                 <ul className="flex flex-col gap-5 mt-8">
